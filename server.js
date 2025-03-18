80e2870e7d20f12c8cb2a01b9ef48bd5a01104b8
dregh1 <rakotonirinacharlesandrea@gmail.com>
2025-03-18 08:02:02 +0300

    login -admin-client-employe

--- server.js
+++ server.js
@@ -6,19 +6,22 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-    // Middleware
-    app.use(cors());
-    app.use(express.json());
-    
-    // Connexion à MongoDB
-    mongoose.connect(process.env.MONGO_URI, {
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-    }).then(() => console.log("MongoDB connecté"))
+}).then(() => console.log("MongoDB connecté"))
     .catch(err => console.log(err));
 
-    // Routes
-    app.use('/services', require('./routes/serviceRoutes'));
-    app.listen(
-        PORT, () => console.log(`Serveur démarré sur le port
+// Routes
+app.use('/services', require('./routes/serviceRoutes'));
+app.use('/login', require('./routes/authRoutes'));
+app.use('/user', require('./routes/userRoutes'));
+
+app.listen(
+    PORT, () => console.log(`Serveur démarré sur le port
     ${PORT}`));
