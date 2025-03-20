99ab5df5796ab0f2871812a293122e552d039970
dregh1 <rakotonirinacharlesandrea@gmail.com>
2025-03-20 11:45:49 +0300

    clean code

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require('dotenv');
 
-// require("dotenv").config();
-
-
 const envFile = `.env.${process.env.NODE_ENV || "development"}`;
 dotenv.config({ path: envFile });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
