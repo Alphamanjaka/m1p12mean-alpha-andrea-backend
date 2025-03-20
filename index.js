4b51387a8bb1a134942e509f365074b10beab9f7
dregh1 <rakotonirinacharlesandrea@gmail.com>
2025-03-20 11:03:17 +0300

    set env dev and prod variable

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require('dotenv');
+
+// require("dotenv").config();
+
+
+const envFile = `.env.${process.env.NODE_ENV || "development"}`;
+dotenv.config({ path: envFile });
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+// const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
 // Middleware
 app.use(cors());
 app.use(express.json());
